5b01c5eaa66db430b48a7778bebeecd1ef35ea35
hefei666 <1379528416@qq.com>
2019-08-29 16:40:28 +0800

    first springcloud

---
+++ cloud_service/src/main/java/com/example/cloud_service/CloudServiceApplication.java
@@ -0,0 +1,16 @@
+package com.example.cloud_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class CloudServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudServiceApplication.class, args);
+    }
+
+}
---
+++ cloud_sp1/src/main/java/com/example/cloud_sp1/CloudSp1Application.java
@@ -0,0 +1,31 @@
+package com.example.cloud_sp1;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class CloudSp1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudSp1Application.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam String name)
+    {
+        return "hi " + name + ",i am from port:" + port;
+    }
+
+
+
+}
